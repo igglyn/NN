8095ec0bc341ce456dfd5b50747d11c522430e3b
igglyn <igglynchan@gmail.com>
2025-04-30 01:55:39 -0700

    WIP rewrite: multitargeting neurons

--- src/neurtest.py
+++ src/neurtest.py
@@ -1,26 +1,29 @@
 import numpy as np
 from time import time
 
-from neurray import Neurray, unpackbits
+from neurray2 import Neurray, unpackbits
 
 if __name__ == "__main__":
-    args = (1, 2, np.uint8, np.uint8)
+    args = (1, 1, np.uint8, np.uint8)
+    #assert args[1] > 1
     eliv = Neurray(*args)
     given = np.empty((args[0],args[1]), dtype=args[2])
     target = np.empty((args[0],args[1]), dtype=args[3])
     
     prev_fitness = -1
-    result = None
     generations = 0
     mutations = 0
 
-    given[0] = 42, 24
+    given[0] = 42 
     #given[1] = 69
 
     target[0] = 69
     #target[1] = 42
 
     eliv.init_array()
+    eliv.allocate_training()
+
+    max_fitness = np.iinfo(args[3]).bits * args[0] * args[1]
 
     start = time()
     while 1:
@@ -29,22 +32,22 @@ if __name__ == "__main__":
         result = eliv.forward(given)
         fitness = np.sum(unpackbits(~np.bitwise_xor(result, target)))
         generations += 1
-        if fitness == np.iinfo(args[3]).bits * args[0] * args[1]:
+        if fitness == max_fitness:
             mutations += 1
             break
         if fitness > prev_fitness:
             prev_fitness = fitness
             mutations += 1
-        eliv.backward(given, target)
+            #print(eliv.forward(given))
+        eliv.backward(target)
 
     end = time()
     print(f"{generations}/{mutations}")
-    print(f"{end - start} seconds")
+    print(f"{round(end - start, 4)} seconds")
     #print(eliv.arrays)
     results = {}
 
     
-    print(eliv.forward(np.array((42), dtype=args[2])))
 
 
 
