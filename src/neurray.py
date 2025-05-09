488561e35ccb49d4ae4ac376395eb9b703fcf90c
igglyn <igglynchan@gmail.com>
2025-05-08 17:03:10 -0700

    backward backwards

--- src/neurray.py
+++ src/neurray.py
@@ -1,130 +1,145 @@
 import numpy as np
+from typing import Optional
 
 gen = np.random.default_rng()
 randarray = lambda size, dtype: gen.integers(np.iinfo(dtype).max, size=size, dtype=dtype)
 
 
 class Neurray:
-    def __init__(self, neurons:int, batch_size:int, idtype=np.uint8, odtype=np.uint8):
+    def __init__(self, neurons:int, idtype=np.uint8, odtype=np.uint8):
         self.neurons = neurons
-        self.batch_size = batch_size
         self.idtype = idtype
         self.odtype = odtype
 
         self.training = False
 
-        self.match = np.empty((4, neurons), dtype=idtype)
-        self.emit = np.empty((4, neurons), dtype=odtype)
+        self.match = np.empty((4, neurons, 1), dtype=idtype)
+        self.emit = np.empty((4, neurons, 1), dtype=odtype)
 
-        # Could be local if no backwards pass, but decided to always handle it here
-        self.choices = np.empty((4, neurons, batch_size), dtype=bool)
-
-    def allocate_training(self):
+    def set_training(self):
         self.training = True
-
-        # inputs and outputs
-        self.givens = np.empty((self.neurons, self.batch_size), dtype=self.idtype)
-        self.results = np.empty((self.neurons, self.batch_size), dtype=self.odtype)
-
-        # batch_size convergance, opted to just keep these around
-        self.neuron_window = np.empty((self.neurons), dtype=self.odtype)
-        self.match_window = np.empty((4, self.neurons), dtype=self.idtype)
-        self.emit_window = np.empty((4, self.neurons), dtype=self.odtype)
 
     def init_array(self, *args):
         # either pass in the arrays or generate some
         if not len(args):
-            self.match[...] = randarray((4, self.neurons), self.idtype)
-            self.emit[...] = randarray((4, self.neurons), self.odtype)
+            self.match[...] = randarray((4, self.neurons, 1), self.idtype)
+            self.emit[...] = randarray((4, self.neurons, 1), self.odtype)
         else:
             self.match[...] = args[0]
             self.emit[...] = args[1]
 
     @property
     def base(self) -> np.ndarray:
-        return np.zeros((self.neurons, self.batch_size), dtype=self.odtype)
+        return np.zeros((self.neurons, 1), dtype=self.odtype)
 
     def forward(self, inputs:np.ndarray) -> np.ndarray:
 
-        # self.choices doesn't have to be stored for only inference
-        # but the array is going to be created over and over again
-
-        # NOTE could be collapsed if desired, but kept for ease of testing atm
-        ## Something is preventing this from being collapsed atm
-
-        match = np.reshape(self.match, (4, self.neurons, 1))
-
-        # check for resonance
-        self.choices[...] = (inputs & match) == 0
+        # check for resonance (saving for backwards)
+        self.choices = (inputs & self.match) == 0
 
         # invert mask based on choice made
-        hidden_states = np.where(self.choices, (emits := np.reshape(self.emit, (4,self.neurons,1))), ~emits)
+        hidden_states = np.where(self.choices, self.emit, ~self.emit)
 
         # start from zero, applying each part
-        ## updating in place when possible
-        outputs = np.copy(self.base)
-
-        outputs ^= hidden_states[0]
-        outputs[...] = ~outputs
-
-        outputs |= hidden_states[1]
-        outputs[...] = ~outputs
-
-        outputs |= hidden_states[2]
-
-        outputs ^= hidden_states[3]
+        outputs = \
+                (((np.copy(self.base)
+                ^ ~hidden_states[0])
+                | ~hidden_states[1])
+                | hidden_states[2]) \
+                ^ hidden_states[3]
 
         # capture for backwards pass
         if self.training:
-            self.givens[...] = inputs
-            self.results[...] = outputs
+            self.givens = inputs
+            self.results = outputs
 
         # Outputs are not merged into a single value and should be treated as masks on previous steps
         return outputs
 
-    def backward(self, target:np.ndarray) -> None:
+    def backward(self, target:np.ndarray, neurons:Optional[np.ndarray]=None) -> None:
         assert self.training, "NN Inputs and Outputs are non existant, place this class in training mode first!"
         
+        batch_size = self.results.shape[1]
         # check if neurons have already converged, inverting the result to use as a mask
         output_diff = self.results ^ target
-        self.neuron_window[...] = output_diff[...,0]
-        for batch in range(self.batch_size-1):
-            self.neuron_window |= output_diff[...,batch+1]
-        neuron_convergance = np.reshape((self.neuron_window != 0), (1, self.neurons))
+        targets = np.copy(target)
+        
+        # TODO isolate search to neurons if present
+
+        # check if neurons already are converged
+        ## useless with max batch size of 2
+        for batch in range(batch_size-1):
+            output_diff[...,0] |= output_diff[...,batch+1]
+            targets[...,0] |= targets[...,batch+1]
+
+        neuron_convergance = (output_diff[...,0] != 0)
+
+        # apply overide
+        if not neurons is None:
+            neuron_convergance &= neruons
+
+        # filter out only the parts that need to be edited
+        filtered_emit = self.emit[:,neuron_convergance]
+        filtered_match = self.match[:,neuron_convergance]
+        filtered_choices = self.choices[:,neuron_convergance]
+
+        filtered_givens = self.givens[neuron_convergance]
+        filtered_results = self.results[neuron_convergance]
+
+        filtered_targets = targets[neuron_convergance, 0, None]
+        filtered_output_diff = output_diff[neuron_convergance, 0, None]
+
+        filtered_target = target[neuron_convergance]
+
+        filtered_length = filtered_results.shape[0]
 
         # select parts to mutate
-        emit_decay  = gen.choice((True,False), size=(4,self.neurons))
-        match_decay = gen.choice((True,False), size=(4,self.neurons))
+        #emit_decay  = gen.choice((True,False), size=(4,filtered_length,1))
+        match_decay = gen.choice((True,True), size=(4,filtered_length,1))
 
-        # select parts to add noise to
-        emit_hurl_decay  = gen.choice((True,False), size=(4,self.neurons))
-        match_hurl_decay = gen.choice((True,False), size=(4,self.neurons))
-        
-        # mask out completed neurons
-        emit_loss_mask  = emit_decay & neuron_convergance
-        match_loss_mask = match_decay & neuron_convergance
+        #emit_loss_mask  = emit_decay
+        match_loss_mask = match_decay
 
-        emit_hurl_mask = emit_decay & emit_hurl_decay & neuron_convergance
-        match_hurl_mask = match_decay & emit_hurl_decay & neuron_convergance
 
         # invert effects of matches that got inverted during forward pass
-        match_states = np.where(self.choices, ~(matches := self.match.reshape(4,self.neurons,1)), matches)
-
+        match_states = np.where(filtered_choices, ~(matches := filtered_match), matches)
+        
         # Calculate diffs between expected and actual outputs
-        expected_match = ~(self.givens ^ match_states)
-        expected_emit  = ~(self.results ^ target)
+        expected_match = ~(filtered_givens ^ match_states)
+        expected_emit  = (~(filtered_results ^ filtered_target)).reshape(1, *filtered_results.shape)
+
+        ## NOTE None stops numpy from eating the dim / regerates the dim
+        match_window = expected_match[...,0,None]
+        emit_window = expected_emit[...,0,None]
 
         # handle batch size (this whole thing exists when it can't converge to more than 2 values)
-        self.match_window[...] = expected_match[...,0]
-        self.emit_window[...]  = expected_emit[...,0]
-        for batch in range(self.batch_size-1):
-            self.match_window &= expected_match[...,batch+1]
-            self.emit_window &= expected_emit[...,batch+1]
+        for batch in range(batch_size-1):
+            next_match = expected_match[...,batch+1,None]
+            next_emit = expected_emit[...,batch+1,None]
+            match_window = (match_window & next_match) ^ ~(match_window | ~next_match)
+            emit_window = (emit_window & next_emit) ^ ~(match_window | ~next_emit)
 
-        # apply weight decay
-        self.match_window[...] = np.where(match_hurl_decay, (self.match_window | randarray((4, self.neurons), self.idtype)), self.match_window)
-        self.emit_window[...] = np.where(emit_hurl_decay, (self.emit_window | randarray((4, self.neurons), self.odtype)), self.emit_window)
+    
+        emit_loss_mask = (~(filtered_emit ^ emit_window) ^ filtered_targets) < (filtered_emit ^ filtered_targets)
+
+        print(emit_loss_mask[...,0,0])
+
 
         # update the neurons
-        self.match[...] = np.where(match_loss_mask, ~(self.match ^ self.match_window), self.match)
-        self.emit[...]  = np.where(emit_loss_mask,  ~(self.emit ^ self.emit_window), self.emit)
+        self.match[...,neuron_convergance,0,None] = np.where(match_loss_mask, ~(filtered_match ^ match_window), filtered_match)
+        self.emit[...,neuron_convergance,0,None]  = np.where(emit_loss_mask,  ~(filtered_emit ^ emit_window), filtered_emit)
+
+
+
+class Elivrge:
+    def __init__(self, step_count:int):
+        self.steps = None 
+
+    @property
+    def base(self) -> np.ndarray:
+        return np.zeros((self.neurons, 1), dtype=self.dtype)
+
+    def forward(self, array):
+        batch_size = array.shape[1]
+
+
--- src/neurtest.py
+++ src/neurtest.py
@@ -3,58 +3,47 @@ from time import time
 
 from neurray import Neurray
 
-def unpackbits(x):
-    """
-    Reshapes the np.unpackbits function from a 1d binary array into the orginal dims + bits
-    """
-    bits = np.iinfo(x.dtype).bits
-    bytes = bits // 8
-    rshape = (*x.shape, bits)
-    output = np.unpackbits(x.astype(f">u{bytes}").view("u1")).reshape(rshape)
-    return output
-
 if __name__ == "__main__":
-    args = (10000, 2, np.uint8, np.uint8)
+    args = (1, np.uint8, np.uint8)
+    args2 = (2,)
     eliv = Neurray(*args)
-    given = np.empty((args[0],args[1]), dtype=args[2])
-    target = np.empty((args[0],args[1]), dtype=args[3])
+    given = np.empty((args[0],args2[0]), dtype=args[1])
+    target = np.empty((args[0],args2[0]), dtype=args[2])
     
-    prev_fitness = -1
+    prev_result = None
     generations = 0
     mutations = 0
 
     given[...,0] = 69
-    given[...,1] = 42
+    if args2[0] >= 2:
+        given[...,1] = 42
+    if args2[0] >= 3:
+        given[...,2] = 0
     target[...,0] = 42
-    target[...,1] = 69
+    if args2[0] >= 2:
+        target[...,1] = 69
+    if args2[0] >= 3:
+        target[...,2] = 255
 
     eliv.init_array()
-    eliv.allocate_training()
-
-    max_fitness = np.iinfo(args[3]).bits * args[0] * args[1]
+    eliv.set_training()
 
     start = time()
-    while 1:
+    for i in range(10):
+    #while 1:
+    #if 1:
         result = eliv.forward(given)
-        fitness = np.sum(unpackbits(~np.bitwise_xor(result, target)))
         generations += 1
-        # The NN should actually stop mutating once it reaches this point
-        if fitness == max_fitness:
+        if not prev_result is None and np.array_equal(result, target):
+            #pass
             break
-        #if fitness > prev_fitness:
-        #    # debugging
-        #    prev_fitness = fitness
+        else:
+            prev_result = result
         eliv.backward(target)
 
     end = time()
-    print(f"""{args[0]} neuron{'s' if args[0]-1 else ''} | {args[1]} target{'s' if args[1]-1 else ''} | {generations} pass{'es' if generations-1 else ''} | {round((end - start) * 1_000, 2)} ms""")
+    print(f"""{args[0]} neuron{'s' if args[0]-1 else ''} | {args2[0]} target{'s' if args2[0]-1 else ''} | {generations} pass{'es' if generations-1 else ''} | {round((end - start) * 1_000, 2)} ms""")
+    print(eliv.forward(given)[0])
+    print(np.unpackbits(eliv.forward(given)[0] ^ target[0]).reshape(args2[0],np.iinfo(args[2]).bits))
    
     
-    results = {}
-    for i in range(np.iinfo(args[2]).max):
-        index = int(eliv.forward(np.array((i,), dtype=args[2]))[0,0])
-        if index in results:
-            results[index] += 1
-        else:
-            results[index] = 0
-    #print(len(results))
